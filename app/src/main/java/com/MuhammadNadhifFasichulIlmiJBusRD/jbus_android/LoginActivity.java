379e034df121f5af60ae25ed445f9c19df435d34
NadhiefIlmi <fasichulilmi2003@gmail.com>
2023-11-17 11:53:21 +0700

    CS08

--- app/src/main/java/com/MuhammadNadhifFasichulIlmiJBusRD/jbus_android/LoginActivity.java
+++ app/src/main/java/com/MuhammadNadhifFasichulIlmiJBusRD/jbus_android/LoginActivity.java
@@ -2,13 +2,47 @@ package com.MuhammadNadhifFasichulIlmiJBusRD.jbus_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button registerNow = null;
+    private Button LoginButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+        // Load the components to the variables
+        registerNow = findViewById(R.id.register_button);
+        LoginButton = findViewById(R.id.login_button);
+
+        registerNow.setOnClickListener(v -> {
+            moveActivity(this, RegisterActivity.class);
+            viewToast(this, "REGISTER AKUN ANDA");
+        });
+
+        LoginButton.setOnClickListener(v -> {
+            moveActivity(this, MainActivity.class);
+            viewToast(this, "SELAMAT DATANG DI JBUS");
+        });
     }
+
+    private void moveActivity(Context ctx, Class<?> cls) {
+        Intent intent = new Intent(ctx, cls);
+        startActivity(intent);
+    }
+
+    private void viewToast(Context ctx, String message) {
+        Toast.makeText(ctx, message, Toast.LENGTH_SHORT).show();
+    }
+
+
+
 }
